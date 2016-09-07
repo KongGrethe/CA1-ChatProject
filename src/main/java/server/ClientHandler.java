0e0285294f747b409877be911b8346bc2cc7609b
Christian <coej94@hotmail.com>
2016-09-07 13:37:13 +0200

    Comments to all methods are written in ClientHandler

--- src/main/java/server/ClientHandler.java
+++ src/main/java/server/ClientHandler.java
@@ -30,30 +30,44 @@ public class ClientHandler extends Thread {
     }
 
     @Override
-    public void run() {
+    public void run() { //The ClientHandler needs a run method, because it 
+                        //extends Thread.
         try {
+            //Declares the OutputStream.
             writer = new PrintWriter(socket.getOutputStream(), true);
+            //Declares the InputStream.
             input = new Scanner(socket.getInputStream());
+            //Writes Greeting Message from the server to the client.
             writer.println(server.getServerGreeting());
+            
             while (true) {
                 try {
                     message = input.nextLine(); // Blocker
                     String[] inputsFromClient = message.split(":");
                     
+                    //This block of code is a CommandSwitch. It handles the
+                    //different possible commands.
                     System.out.println("Command is " + inputsFromClient[0]);
                     inputsFromClient[0] = inputsFromClient[0].toUpperCase();
                     switch (inputsFromClient[0]) {
+                        //Login command
                         case "LOGIN":
                             setUserName(inputsFromClient[1]);
                             writer.println(server.getClientList());
                             break;
+                            
+                        //Message command
                         case "MSG":
+                            //Creates an array with recipients of a message and
+                            //split the names with a ",".
                             String[] recipients = inputsFromClient[1].split(",");
                             System.out.println("There are " + recipients.length + " recipients");
+                            //Creates a String called text. 
                             String text = inputsFromClient[2];
                             System.out.println("The text is " + text);
+                            //If no recipients are given, the message is sent to
+                            //all online users on the server.
                             if (recipients[0].equals("")) {
-                                // Send to all clients
                                 System.out.println("Send this to all clients");
                                 server.sendToAllClients(text, username);
                             } else {
@@ -61,17 +75,23 @@ public class ClientHandler extends Thread {
                                 server.sendSpecific(recipients, text, username);
                             }
                             break;
+                            
+                        //Logout Command
                         case "LOGOUT":
                             writer.println("Signed out.");
                             socket.close();
                             break;
+                        //If an unknown command is written, the default case
+                        //will be run.
                         default:
                             System.out.println("The command was ");
                             writer.println("Unknown command. Please reply with proper protocol"
                                     + "<\n Ex: MSG:<message>  LOGIN:<username>  LOGOUT: ");
                             break;
                     }
-
+                    //If a person in the chat writes invalid commands, this 
+                    //catch block will handle the exception and write a message 
+                    //to the user about the problem.
                 } catch (ArrayIndexOutOfBoundsException ex) {
                     System.out.println(ex.getMessage());
                     writer.println("The protocol is: MSG:<recipients seperated by ,>:<message> \n"
@@ -80,10 +100,14 @@ public class ClientHandler extends Thread {
             }
         } catch (IOException e) {
             System.out.println("oh no");
+        //If the user shut down his terminal window, this exteption will be 
+        //caught and a message is printed to the output in netbeans.
         } catch (NoSuchElementException e2) {
             System.out.println("Client closed window probably");
             server.removeClient(this);
-
+        
+        //No matter how the runmethod ends, this finally will be executed.
+        //It will close the socket.
         } finally {
             try {
                 socket.close();
@@ -92,17 +116,20 @@ public class ClientHandler extends Thread {
             }
         }
     }
-
+    
+    //Writes the message, flushes and print to Output "Send message"
     public void sendMessage(String msg) {
         writer.println(msg);
         writer.flush();
         System.out.println("Send message!");
     }
 
+    //UsernameSette.
     private void setUserName(String name) {
         username = message = name;
     }
-
+    
+    //UsernameGetter
     public String getUserName() {
         return username;
     }
