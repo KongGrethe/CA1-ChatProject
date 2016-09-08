d860da5f69aafee3d556b6e0422197e933cc7d67
Patrick Johansen <Patrick Johansen>
2016-09-08 10:16:56 +0200

    Removed sout messages from the server and clienthandler classes

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -6,8 +6,6 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class Client {
 
--- src/main/java/server/Server.java
+++ src/main/java/server/Server.java
@@ -34,7 +34,6 @@ public class Server {
             //
             serverSocket.bind(new InetSocketAddress(myIP, myPort));
 
-            System.out.println("Server listens on " + myPort + " at " + myIP);
 
             //While the server is running the code below runs.
             while (keepRunning) {
@@ -59,15 +58,11 @@ public class Server {
     //This method adds client to list of clients.
     public void addClient(ClientHandler client) {
         clients.add(client);
-        System.out.println("A user has joined!");
-        System.out.println("Total number of users is: " + clients.size());
     }
 
     //This method removes client to list of clients.
     public void removeClient(ClientHandler client) {
         clients.remove(client);
-        System.out.println("A user has left!");
-        System.out.println("Total number of users is: " + clients.size());
     }
 
     //This method sends a message to all users online.
